cb928a8dbc4f912747694e2e2d467c6efac420f6
tasos <akkell7@gmail.com>
2020-03-30 15:33:08 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import React from "react";
-import { Admin, Resource, ListGuesser } from "react-admin";
-import restProvider from "ra-data-simple-rest";
-
-import { BuildingList } from "./components/BuildingList";
 
 function App() {
-  return (
-    <Admin dataProvider={restProvider("http://localhost:5000/api")}>
-      <Resource name="buildings" list={BuildingList} />
-      <Resource name="fees" list={ListGuesser} />
-      <Resource name="users" list={ListGuesser} />
-      <Resource name="statistics" list={ListGuesser} />
-    </Admin>
-  );
+  return <></>;
 }
 
 export default App;
--- src/components/BuildingList.js
+++ src/components/BuildingList.js
@@ -1,13 +1,3 @@
 import React from "react";
-import { List, Datagrid, TextField, EditButton } from "react-admin";
 
-export const BuildingList = props => (
-  <List {...props}>
-    <Datagrid>
-      <TextField source="address.street" />
-      <TextField source="address.streetnumber" />
-      <TextField source="address.area" />
-      <EditButton />
-    </Datagrid>
-  </List>
-);
+export const BuildingList = props => <></>;

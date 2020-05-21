7d87ab41b508a9e4ee0b45d111dbfb68562ecdcc
Evangelos Chavlis <vagchavlis96@gmail.com>
2020-05-21 08:06:54 +0300

    add comments

--- src/components/Admin/BuildingList1.tsx
+++ src/components/Admin/BuildingList1.tsx
@@ -4,44 +4,44 @@ import UserContext from "../../UserContext";
 const uri = process.env.REACT_APP_API_URL + "/api";
 const BuildingList1 = () => {
   const manager = useContext(UserContext);
-  const sort = (
-    ascending: boolean,
-    columnClassName: string,
-    tableId?: string
-  ) => {
-    var tbody = document
-      .getElementById(tableId)
-      .getElementsByTagName("tbody")[0];
-    var rows = tbody.getElementsByTagName("tr");
+  // const sort = (
+  //   ascending: boolean,
+  //   columnClassName: string,
+  //   tableId?: string
+  // ) => {
+  //   var tbody = document
+  //     .getElementById(tableId)
+  //     .getElementsByTagName("tbody")[0];
+  //   var rows = tbody.getElementsByTagName("tr");
 
-    var unsorted = true;
+  //   var unsorted = true;
 
-    while (unsorted) {
-      unsorted = false;
+  //   while (unsorted) {
+  //     unsorted = false;
 
-      for (var r = 0; r < rows.length - 1; r++) {
-        var row = rows[r];
-        var nextRow = rows[r + 1];
+  //     for (var r = 0; r < rows.length - 1; r++) {
+  //       var row = rows[r];
+  //       var nextRow = rows[r + 1];
 
-        var value = row.getElementsByClassName(columnClassName)[0].innerHTML;
-        var nextValue = nextRow.getElementsByClassName(columnClassName)[0]
-          .innerHTML;
+  //       var value = row.getElementsByClassName(columnClassName)[0].innerHTML;
+  //       var nextValue = nextRow.getElementsByClassName(columnClassName)[0]
+  //         .innerHTML;
 
-        value = value.replace(",", ""); // in case a comma is used in float number
-        nextValue = nextValue.replace(",", "");
+  //       value = value.replace(",", ""); // in case a comma is used in float number
+  //       nextValue = nextValue.replace(",", "");
 
-        if (!isNaN(value)) {
-          value = parseFloat(value);
-          nextValue = parseFloat(nextValue);
-        }
+  //       if (!isNaN(value)) {
+  //         value = parseFloat(value);
+  //         nextValue = parseFloat(nextValue);
+  //       }
 
-        if (ascending ? value > nextValue : value < nextValue) {
-          tbody.insertBefore(nextRow, row);
-          unsorted = true;
-        }
-      }
-    }
-  };
+  //       if (ascending ? value > nextValue : value < nextValue) {
+  //         tbody.insertBefore(nextRow, row);
+  //         unsorted = true;
+  //       }
+  //     }
+  //   }
+  // };
 
   console.log("has run");
   const dummyData = [
@@ -81,30 +81,30 @@ const BuildingList1 = () => {
           <tr>
             <th scope="col" className="street">
               Οδός
-              <button onClick={() => sort(true, "street", "content-table")}>
+              {/* <button onClick={() => sort(true, "street", "content-table")}>
                 asc
               </button>
               <button onClick={() => sort(false, "street", "content-table")}>
                 desc
-              </button>
+              </button> */}
             </th>
             <th scope="col" className="number">
               Αριθμός{" "}
-              <button onClick={() => sort(true, "number", "content-table")}>
+              {/* <button onClick={() => sort(true, "number", "content-table")}>
                 asc
               </button>
               <button onClick={() => sort(false, "number", "content-table")}>
                 desc
-              </button>
+              </button> */}
             </th>
             <th scope="col" className="area">
               Περιοχή
-              <button onClick={() => sort(true, "area", "content-table")}>
+              {/* <button onClick={() => sort(true, "area", "content-table")}>
                 asc
               </button>
               <button onClick={() => sort(false, "area", "content-table")}>
                 desc
-              </button>
+              </button> */}
             </th>
           </tr>
         </thead>
